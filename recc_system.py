04d87b16e76e385a5517b7a3f9000a24a5d2eb25
PRAGYA-MISHRAA <pragya.mishra0105@gmail.com>
2025-07-07 12:30:13 +0530

    Create recc_system.py

---
+++ recc_system.py
@@ -0,0 +1,25 @@
+movies = {
+    "Mad Max": ["action", "adventure"],
+    "Die Hard": ["action", "thriller"],
+    "Titanic": ["romance", "drama"],
+    "Notebook": ["romance", "drama"],
+    "Inception": ["sci-fi", "thriller"],
+    "Interstellar": ["sci-fi", "drama"]
+}
+
+# user's preferences
+likes = input("Which genres do you like ? (comma separated) ").lower().split(",")
+
+# Clean up spaces
+likes = [g.strip() for g in likes]
+
+print("\nRecommended for you:")
+
+found = False
+for movie, genres in movies.items():
+    if any(g in genres for g in likes):
+        print("-", movie)
+        found = True
+
+if not found:
+    print("Sorry, no matches found.")
